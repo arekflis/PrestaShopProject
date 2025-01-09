cc1339ab26a666f52ae45db3e2bd48fe4beda185
arekflis <arkadiuszflisikowski10@gmail.com>
2025-01-09 22:35:31 +0100

    Change parameters.php

--- shop/prestashop_data/app/config/parameters.php
+++ shop/prestashop_data/app/config/parameters.php
@@ -1,11 +1,11 @@
 <?php return array (
   'parameters' => 
   array (
-    'database_host' => 'mysql_193480',
+    'database_host' => getenv('DB_SERVER') ?: 'admin-mysql_db',
     'database_port' => '',
-    'database_name' => 'BE_19348',
-    'database_user' => 'root',
-    'database_password' => 'student',
+    'database_name' => getenv('DB_NAME') ?: 'BE_19348',
+    'database_user' => getenv('DB_USER') ?: 'root',
+    'database_password' => getenv('DB_PASSWD') ?: 'student',
     'database_prefix' => 'ps_',
     'database_engine' => 'InnoDB',
     'mailer_transport' => 'smtp',
@@ -13,7 +13,7 @@
     'mailer_user' => NULL,
     'mailer_password' => NULL,
     'secret' => '4DMq2bKyVctIENLZowkzZcmZg4m3Rv3hPrPYpE50iBpsESq1qQAWZtZrvAK0mdET',
-    'ps_caching' => 'CacheMemcached',
+    'ps_caching' => 'CacheMemcache',
     'ps_cache_enable' => true,
     'ps_creation_date' => '2024-10-23',
     'locale' => 'pl-PL',
@@ -21,5 +21,6 @@
     'cookie_key' => 'O38xZ2tP8dLARXVIteGTAcCJf9IBNMPg8ZGURSbTDO0VYk4JfuxsmZ7BLdB5hmDB',
     'cookie_iv' => 'K980bf5iuXg3B9W6055Q3UMT7S4f1f5Q',
     'new_cookie_key' => 'def00000970d0bf2aca012439e042d5d72433bd4ebfeab1be1a1022940edd79ba60622e8fbe4eb014d8cfbaaabacc76efe5f59e23bd4e3a052b01fcd0653f2c3d493d66f',
+
   ),
 );
